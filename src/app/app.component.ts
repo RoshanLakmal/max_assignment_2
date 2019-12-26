e13f25e2ef87300c5cebc9fd6f0e925863d1c4a4
Roshan Lakmal <roshanlakmal@Roshans-MacBook-Air.local>
2019-12-26 13:59:54 +1100

    refector for simplicity

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,22 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  allowReset = false;
   username = "";
-  usernameValue = "";
-
-  constructor(){
-  
-  }
-  
-  onUpdateInput(event: Event) {
-    this.username = (<HTMLInputElement>event.target).value;
-    if(this.username != ""){
-      this.allowReset = true;
-    }
-  }
-  onReset() {
-    this.username = "";
-    this.usernameValue = "";
-  }
 }
